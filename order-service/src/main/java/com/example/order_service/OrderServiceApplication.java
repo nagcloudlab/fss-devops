8bfc938019fcce8515349ec530e4bbec47ffbb55
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-12-19 22:14:47 +0530

    cors

--- order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
