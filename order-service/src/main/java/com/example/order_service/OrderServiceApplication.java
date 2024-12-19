3f6dd391d4ef68e2b379a1f098b07529459100bb
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-12-19 22:12:33 +0530

    podman services

--- order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -1,18 +1,10 @@
-package com.example;
+package com.example.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class OrderServiceApplication {
-
-	@PostMapping("/order")
-	public String order() {
-		return "Order is placed successfully!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
--- shop-ui-service/src/app/app.component.spec.ts
+++ shop-ui-service/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'shop-ui' title`, () => {
+  it(`should have the 'shop-ui-service' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shop-ui');
+    expect(app.title).toEqual('shop-ui-service');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, shop-ui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, shop-ui-service');
   });
 });
