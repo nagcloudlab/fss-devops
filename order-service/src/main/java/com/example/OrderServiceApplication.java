24dcbe77f286890fdd7ff5b17353214f38393a20
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-12-20 07:07:30 +0530

    fix

--- order-service/src/main/java/com/example/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/OrderServiceApplication.java
@@ -1,8 +1,32 @@
 package com.example;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RestController
+class OrderController {
+
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	@PostMapping("/order")
+	public String placeOrder(@RequestBody List<Integer> productIds){
+		String order = productIds.toString();
+		kafkaTemplate.send("orders", order);
+		return "Order placed successfully";
+	}
+
+}
+
 
 @SpringBootApplication
 @CrossOrigin(origins = "*")
--- product-service/src/main/java/com/example/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/ProductServiceApplication.java
@@ -1,8 +1,62 @@
 package com.example;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "product")
+class Product{
+	@Id
+	private int id;
+	private String name;
+	private int quantity;
+	private double price;
+}
+
+interface ProductRepository extends JpaRepository<Product, Integer> {
+}
+
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RestController
+class ProductController {
+
+	private final ProductRepository repository;
+
+	@PostConstruct
+	void init(){
+		Product p1 = new Product(1, "Laptop", 100, 50000);
+		Product p2 = new Product(2, "Mobile", 200, 20000);
+		Product p3 = new Product(3, "Tablet", 300, 10000);
+		repository.save(p1);
+		repository.save(p2);
+		repository.save(p3);
+	}
+
+	@GetMapping("/products")
+	public List<Product> getProducts(){
+		return repository.findAll();
+	}
+}
+
 
 @SpringBootApplication
 @CrossOrigin(origins = "*")
